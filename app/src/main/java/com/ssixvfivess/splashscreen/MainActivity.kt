44fdb6c1cb536bbf7373a0291d6da5ff4f918ae6
ssixvfivess <ssixvfivess@yandex.ru>
2025-07-28 23:50:19 +0500

    Make animation with dog

--- app/src/main/java/com/ssixvfivess/splashscreen/MainActivity.kt
+++ app/src/main/java/com/ssixvfivess/splashscreen/MainActivity.kt
@@ -1,14 +1,40 @@
 package com.ssixvfivess.splashscreen
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.filament.View
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply{
+            setKeepOnScreenCondition{
+                !viewModel.isReady.value
+            }
+
+//            setOnExitAnimationListener{ screen ->
+//                val zoomX = ObjectAnimator.ofFloat(
+//                    screen.iconView,
+//                    View.SCALE_X,
+//                    0.4f,
+//                    0.0f
+//                )
+//                zoomX.interpolator = OvershootInterpolator()
+//                zoomX.duration = 500L
+//                zoomX.doOnEnd {screen.remove() }
+//            }
+        }
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
